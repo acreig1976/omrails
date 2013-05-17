f3c189bc26d000d6fe88bc3c93dc1ea2ce545670
Andy Creighton <andy.m.creighton@gmail.com>
2013-05-16 21:01:23 -0700

    Add Paperclip Gem More Fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,5 +9,5 @@ class Post < ActiveRecord::Base
 																size: { less_than: 5.megabytes }
 
   belongs_to :user
-  has_attached_file :image, styles: { medium: "300x300"}
+  has_attached_file :image, styles: { thumb: "100x100"}
 end
