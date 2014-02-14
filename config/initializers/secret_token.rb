6b64a408afc4a40011f80e4cf5371b2346e9f12b
Sean Walker <swlkr.rbl@gmail.com>
2014-02-14 00:22:00 -0800

    Use correct config name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Omrails::Application.config.secret_token = '6235339f8900cc7c377e8cf71b7452582092dce2aa7601350e4da1a1791f84a17d07dcef029ade813360bed6baa75735a5004d9c6417c3592cdf427b45c5498b'
-Omrails::Application.config.secret_key = '94781d5049050268f1b4ea32c5211f76babe5492e7582472897463ee718fa707340f8ea1155fb9a6add6fa69ac64abab7bc3818a5a061326cb8934344d6a8629'
+Omrails::Application.config.secret_key_base = '94781d5049050268f1b4ea32c5211f76babe5492e7582472897463ee718fa707340f8ea1155fb9a6add6fa69ac64abab7bc3818a5a061326cb8934344d6a8629'
