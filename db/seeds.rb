e1d2b4652d1128fb8a1422027f00af7fb3afd986
Sean Walker <swlkr.rbl@gmail.com>
2014-02-17 19:28:38 -0800

    Add seed file for multiple users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([
+  {
+    email: 'test1@example.com',
+    password: 'test1234',
+    password_confirmation: 'test1234'
+  },
+  {
+    email: 'test2@example.com',
+    password: 'test1234',
+    password_confirmation: 'test1234'
+  },
+  {
+    email: 'test3@example.com',
+    password: 'test1234',
+    password_confirmation: 'test1234'
+  }
+  ])
